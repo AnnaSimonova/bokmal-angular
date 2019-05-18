bfce161bb6c4184e481faacfc964d10cc32eceb3
Ganna Simonova <anya_simonova@yahoo.com>
2019-05-18 17:42:22 +0300

    Added toggling sidebar and routes for menu items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('slideInOut', [
+      state('in', style({
+        transform: 'translate3d(-100%, 0, 0)'
+      })),
+      state('out', style({
+        transform: 'translate3d(0, 0, 0)'
+      })),
+      transition('in => out', animate('400ms ease-in-out')),
+      transition('out => in', animate('400ms ease-in-out'))
+    ]),
+      trigger('burgerFirstLine', [
+          state('open', style({
+            transform: 'rotate(45deg) translate(5px, 6px)'
+          })),
+        state('close', style({
+          transform: 'rotate(0deg) translate(0, 0)'
+        })),
+        transition('open => close', animate('400ms ease-in-out')),
+        transition('close => open', animate('400ms ease-in-out'))
+      ]),
+    trigger('burgerThirdLine', [
+      state('open', style({
+        transform: 'rotate(-45deg) translate(1px, -2px)'
+      })),
+      state('close', style({
+        transform: 'rotate(0deg) translate(0, 0)'
+      })),
+      transition('open => close', animate('400ms ease-in-out')),
+      transition('close => open', animate('400ms ease-in-out'))
+    ]),
+    trigger('burgerOddLine', [
+      state('open', style({
+        visibility: 'hidden'
+      })),
+      state('close', style({
+        visibility: 'visible'
+      })),
+      transition('open => close', animate('400ms ease-in-out'))
+    ]),
+  ]
 })
 export class AppComponent {
+  menuState:string = 'in';
+  burgerState: string = 'close';
 
+  toggleMenu() {
+    this.menuState = this.menuState === 'out' ? 'in' : 'out';
+    this.burgerState = this.burgerState === 'close' ? 'open' : 'close';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HttpXhrBackend } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { ArticleComponent } from './article/article.component';
@@ -8,17 +9,22 @@ import { ArticleListComponent } from './article-list/article-list.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ArticleService } from "./article.service";
 import { MockXHRBackend } from './mock-xhr-backend';
+import { MenuComponent } from './menu/menu.component';
+import { SectionListComponent } from './section-list/section-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ArticleComponent,
-    ArticleListComponent
+    ArticleListComponent,
+    MenuComponent,
+    SectionListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+      BrowserAnimationsModule
   ],
   providers: [
     ArticleService,
